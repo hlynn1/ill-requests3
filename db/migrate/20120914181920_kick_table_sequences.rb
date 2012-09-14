884abab53d2382d626c8803c2009ab1a19ed9ce6
Lynn Hoffman <lynn_m_hoffman@yahoo.com>
2012-09-14 13:46:46 -0500

    psql db fixes

--- db/migrate/20120914181920_kick_table_sequences.rb
+++ db/migrate/20120914181920_kick_table_sequences.rb
@@ -1,29 +1,8 @@
 class KickTableSequences < ActiveRecord::Migration
   def up
-    connection.execute(%q{
-      select setval('activities_id_seq', max(id))
-      from activities
-    })
-    connection.execute(%q{
-      select setval('customers_id_seq', max(id))
-      from customers
-    })
-    connection.execute(%q{
-      select setval('locations_id_seq', max(id))
-      from locations
-    })
-    connection.execute(%q{
-      select setval('requests_id_seq', max(id))
-      from requests
-    })
-    connection.execute(%q{
-      select setval('statuses_id_seq', max(id))
-      from statuses
-    })
-    connection.execute(%q{
-      select setval('users_id_seq', max(id))
-      from users
-    })
+    execute 'ALTER SEQUENCE activities_id_seq RESTART WITH 159;'
+    execute 'ALTER SEQUENCE requests_id_seq RESTART WITH 80;'
+    execute 'ALTER SEQUENCE customers_id_seq RESTART WITH 93;'
   end
 
   def down
