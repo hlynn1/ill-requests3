3c9b0d63b5219f359ef8e48f68a905cc91709c30
Lynn Hoffman <lynn_m_hoffman@yahoo.com>
2012-09-15 22:26:19 -0500

    cleanup

--- db/migrate/20120814161744_add_remember_token_and_location_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddRememberTokenAndLocationToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :remember_token, :string
-    add_column :users, :locationcode, :string
-    add_index :users, :remember_token
-  end
-end
--- db/migrate/20120819193540_add_activities_index.rb
+++
@@ -1,5 +0,0 @@
-class AddActivitiesIndex < ActiveRecord::Migration
-  def change
-    add_index :activities, [:request_id, :created_at]
-  end
-end
