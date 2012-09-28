b14e7f4bcb9e30fbc9ca4ba1da4fe53153dbdbc0
Lynn Hoffman <lynn_m_hoffman@yahoo.com>
2012-09-28 13:06:54 -0500

    fixed css compile problem

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,9 @@ module IllRequests
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # To address problems with Heroku slug precompile
+    config.assets.intialize_on_precompile = false
+    
   end
 end
